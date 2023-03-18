c5a1692630fedd582dd032de9374b15810c489e2
Maol-1997 <m.ortiz@alumno.um.edu.ar>
2023-03-18 02:35:33 -0300

    optimice css (day 2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
+import './bootstrap.scss'
 import NavBar from './components/NavBar'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function RootLayout ({
   children
