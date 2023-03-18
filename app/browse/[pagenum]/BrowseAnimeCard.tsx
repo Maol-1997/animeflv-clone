1a039389b0ee10b3f464a564a1c97a13a7735c56
Maol-1997 <m.ortiz@alumno.um.edu.ar>
2023-03-18 02:35:33 -0300

    adds browse anime description

--- app/browse/[pagenum]/BrowseAnimeCard.tsx
+++ app/browse/[pagenum]/BrowseAnimeCard.tsx
@@ -6,9 +6,14 @@ import Styles from './BrowseAnimeCard.module.css'
 // @ts-ignore
 export default function AnimeCard ({ anime }) {
   return (
-    <Link href={`/anime/${anime.link}`}>
+    <Link href={`/anime/${anime.link}`} className={Styles.animeContainer}>
       <div className={Styles.imageContainer}>
         <Image data-atropos-offset='-4.5' src={anime.img} alt='' width={260} height={370} style={{ borderRadius: '12px' }} />
+      </div>
+      <div className={Styles.popup}>
+        <span style={{ fontSize: '1rem', fontWeight: 'bold' }}>{anime.title}</span>
+        <br />
+        <span style={{ color: '#B0BEC5' }}>{anime.description}</span>
       </div>
       <div className={Styles.textContainer}>
         <span> {anime.title} </span>
